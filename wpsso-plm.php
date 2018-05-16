517a51b50b7e53eb25c8da676af3bd79b9966014
JS Morisset <jsm@surniaulula.com>
2018-05-16 17:07:14 -0400

    v2.5.3

--- wpsso-plm.php
+++ wpsso-plm.php
@@ -13,7 +13,7 @@
  * Description: WPSSO Core add-on to provide Pinterest Place, Facebook / Open Graph Location, Schema Local Business, and Local SEO meta tags.
  * Requires PHP: 5.4
  * Requires At Least: 3.8
- * Tested Up To: 4.9.5
+ * Tested Up To: 4.9.6
  * Version: 2.5.3
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
