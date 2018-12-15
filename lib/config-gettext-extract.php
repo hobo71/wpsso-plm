2fe1ed8c4f4fc8d593384cb17ebb26b5cd872a13
JS Morisset <jsm@surniaulula.com>
2018-12-15 17:43:30 -0600

    v3.0.5

--- lib/config-gettext-extract.php
+++
@@ -1,8 +0,0 @@
-<?php die( 'These aren\'t the droids you\'re looking for...' );
-// extract of translatable text strings from static config array
-_x( '09:00', 'option value', 'wpsso-plm' );
-_x( '17:00', 'option value', 'wpsso-plm' );
-_x( 'local.business', 'option value', 'wpsso-plm' );
-_x( 'Place / Location', 'lib file description', 'wpsso-plm' );
-_x( 'wp', 'option value', 'wpsso-plm' );
-_x( 'WPSSO Core add-on to provide Pinterest Place, Facebook / Open Graph Location, Schema Local Business, and Local SEO meta tags.', 'plugin description', 'wpsso-plm' );
---
+++ lib/gettext-config.php
@@ -0,0 +1,25 @@
+<?php die( 'These aren\'t the droids you\'re looking for...' );
+
+/**
+ * Extract of translatable text strings from the static config array.
+ */
+_x( 'Place / Location', 'lib file description', 'wpsso-plm' );
+_x( 'WPSSO Core add-on to provide Pinterest Place, Facebook / Open Graph Location, Schema Local Business, and Local SEO meta tags.', 'plugin description', 'wpsso-plm' );
+_x( 'local.business', 'option value', 'wpsso-plm' );
+_x( 'wp', 'option value', 'wpsso-plm' );
+_x( '09:00', 'option value', 'wpsso-plm' );
+_x( '17:00', 'option value', 'wpsso-plm' );
+_x( '09:00', 'option value', 'wpsso-plm' );
+_x( '17:00', 'option value', 'wpsso-plm' );
+_x( '09:00', 'option value', 'wpsso-plm' );
+_x( '17:00', 'option value', 'wpsso-plm' );
+_x( '09:00', 'option value', 'wpsso-plm' );
+_x( '17:00', 'option value', 'wpsso-plm' );
+_x( '09:00', 'option value', 'wpsso-plm' );
+_x( '17:00', 'option value', 'wpsso-plm' );
+_x( '09:00', 'option value', 'wpsso-plm' );
+_x( '17:00', 'option value', 'wpsso-plm' );
+_x( '09:00', 'option value', 'wpsso-plm' );
+_x( '17:00', 'option value', 'wpsso-plm' );
+_x( '09:00', 'option value', 'wpsso-plm' );
+_x( '17:00', 'option value', 'wpsso-plm' );
