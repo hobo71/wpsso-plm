b875326f0178f78d69c286272a3d1cd966918ecc
JS Morisset <jsm@surniaulula.com>
2019-04-05 14:39:57 -0600

    v3.0.6

--- wpsso-plm.php
+++ wpsso-plm.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO Core add-on provides Pinterest Place, Facebook / Open Graph Location, Schema Local Business and Local SEO meta tags.
  * Requires At Least: 3.8
- * Tested Up To: 5.1
+ * Tested Up To: 5.1.1
  * Version: 3.0.6
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
